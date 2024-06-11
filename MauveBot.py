09655541c9c6214580d2b35dcb88cfb190f94f05
AutumnSkies <147667005+AutumnSkies@users.noreply.github.com>
2024-06-10 20:17:22 -0400

    Update MauveBot.py
    Added embeds for mass role change and stuff
    
    Added logging which is broken

--- MauveBot.py
+++ MauveBot.py
@@ -1,12 +1,13 @@
 import discord
 from discord.ext import commands
 import asyncio
+import logging
 
 # Replace 'token here' with the token
 
 TOKEN = 'token here'
 
-# Just intents stuff
+# Just intents stuff (basically defines bot specific permissions)
 
 intents = discord.Intents.default()
 intents.members = True
@@ -14,11 +15,15 @@ intents.message_content = True
 
 description = '''You probably shouldn't use this if you don't know what it does.
 
-En masse role replacer made by catgirlandamoth (Sadie). Special thanks to Pea, Diane and all the rest on the rain discord for help!'''
+En masse role replacer made by catgirlandamoth (Sadie). Special thanks to Pea, Diane and all the rest on the rain discord for help!
+
+If you're having issues with permissions did you create and assign the "MauvePermissions" role? If you're still having issues, consider rate limits or server size. Patience is a virue :3'''
+
+# Sets up prefix "m;" description and intents
 
 bot = commands.Bot(command_prefix='m;', description=description, intents=intents)
 
-# Sends a message in terminal to let us know the bot should be acceping commands
+# Sends a message in terminal to let us know the bot should be online, and thus accepting commands.
 # Also sets presence. Lavender really is a pretty color.
 
 @bot.event
@@ -34,6 +39,13 @@ async def on_command_error(ctx, error):
     if isinstance(error, discord.ext.commands.errors.MissingRole) and "Role 'MauvePermissions' is required to run this command" in str(error):
         await ctx.send("I'm pretty sure you shouldn't be messing with this <3 (Lacking MauvePermissions role)")
 
+# Configure logging - does this actually work? it's not writing to Mauve.log
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('Mauve')
+handler = logging.FileHandler(filename='Mauve.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
 # Basic ping command
 
 @bot.command()
@@ -41,9 +53,11 @@ async def ping(ctx):
      await ctx.send(f'Pong! {round(bot.latency * 1000)}ms')
 
 # Index command: Use caution on large servers. May lag and/or ratelimit
-# I have goddamn idea how this works on larger servers. Probably need to put in something that makes this bot ratelimit itself
+# I have no goddamn idea how this works on larger servers. Probably need to put in something that makes this bot ratelimit itself
+# I think discord.py does rate limit internally though?. RUN INDEX COMMAND FIRST BEFORE PUSHING THE BIG RED BUTTON TO CHECK THIS
 
 @bot.command()
+# You'll see this a lot, it makes sure that the user has the proper permissions role. Without that role the command wont execute for god themselves
 @commands.has_role("MauvePermissions")
 async def index(ctx):
     # There has got to be a more effecient way to do this
@@ -71,6 +85,8 @@ async def index(ctx):
 # It cannot be this fucking easy to do this
 # Anyway, here we map out the roles we want gone and which ones replace them.
 # Maybe move this to the top later?
+# YOU WILL NEED TO CHANGE ALL OF THESE TO MATCH WHATEVER YOUR SERVER LOOKS LIKE
+# Yes, it's tedious, sorry!
 
 role_mappings = {
     "LegacyPronoun1": ("NewPronoun1", "color1"),
@@ -87,11 +103,13 @@ role_mappings = {
     "LegacyPronoun12": ("NewPronoun12", "color12"),
 }
 
+
 # This is the actual replacement script
 
 @bot.command()
 @commands.has_role("MauvePermissions")
 async def update_roles(ctx):
+    # It's the litteral big red button, of course there's a confirmation
     confirmation_message = await ctx.send("You have 60 seconds to confirm this command. React with ✅ to confirm or ❌ to cancel.")
 
     await confirmation_message.add_reaction("✅")
@@ -99,11 +117,11 @@ async def update_roles(ctx):
 
     def check(reaction, user):
         return user == ctx.author and str(reaction.emoji) in ["✅", "❌"]
-
+    # Cool, you confirmed the command! Now enjoy the wait.
     try:
         reaction, user = await bot.wait_for('reaction_add', timeout=60.0, check=check)
         if str(reaction.emoji) == "✅":
-            await ctx.send("Confirmed. This will take a while. I am not kidding.")
+            await ctx.send("Confirmed. This will take a while. Progress is displayed in terminal")
             guild = ctx.guild
             for member in guild.members:
                 for role_name, (new_role_name1, new_role_name2) in role_mappings.items():
@@ -113,17 +131,22 @@ async def update_roles(ctx):
                     if role in member.roles:
                         await member.remove_roles(role)
                         await member.add_roles(new_role1, new_role2)
+                        # prints to terminal
                         print(f"Updated roles for {member.name}: removed {role_name}, added {new_role_name1} and {new_role_name2}")
-            await ctx.send("Roles have been updated successfully.")
+                        # prints to channel
+                        embed = discord.Embed(
+                                                title=f"Updated roles for {member.name}",
+                                                description=f"Removed {role_name}, added {new_role_name1} and {new_role_name2}",
+                                                color=discord.Color.purple()
+                                                                                )
+                        await ctx.send(embed=embed)
+            await ctx.send("Roles have been updated successfully!")
         else:
+            # I think my jokes are funny. Do you?
             await ctx.send("Canceled. You live to confirm another day :3")
     except asyncio.TimeoutError:
+        # Every 60 seconds in New York a minute passes in Virginia
         await ctx.send("Canceled. Timeout.")
 
-
-
-# VERY FANCY LOG CODE GOES HERE ONCE I FINISH IT
-
-
-
-bot.run(TOKEN)
+#pulls token from up top and should actually make the log handler work
+bot.run(TOKEN, log_handler=handler)
